494a7536365e63ab56adf765e20c6c0b149ce245
sumitradas94 <sumitradas94@gmail.com>
2022-12-27 18:56:52 +0530

    Modified for UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Line comparison competation program");
+            Linelenth.lenth();
         }
     }
 }
