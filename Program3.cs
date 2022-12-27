98dc9beb97dfba92b11c6594d94f2796dad3f109
sumitradas94 <120413479+sumitradas94@users.noreply.github.com>
2022-12-27 20:03:40 +0530

    Modifieduc3

---
+++ Program3.cs
@@ -0,0 +1,13 @@
+namespace Linecomparisonproblem
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Line comparison competation program");
+            //Linelenth.lenth();
+            //TwilineLength.Twoline();
+            EndpointLength.Point();
+        }
+    }
+}
