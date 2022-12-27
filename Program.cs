d69c7c361997fbcada27ece562c79ff23c539347
sumitradas94 <sumitradas94@gmail.com>
2022-12-27 18:27:46 +0530

    greeting

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Linecomparisonproblem
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Line comparison competation program");
+        }
+    }
+}
